48316477c886700cc0da67c1b3434c10a69b0847
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-11-01 13:40:15 +0100

    started work on moving forward based on head bobbing

--- tardis-world/scripts/infinite_objects.js
+++ tardis-world/scripts/infinite_objects.js
@@ -3,6 +3,7 @@
 AFRAME.registerComponent('update_trees',{
     tick: function () {
         var scene = document.querySelector('a-scene');
+        console.log(this.el)
         var objects = scene.querySelectorAll('#gentree')
         var player = document.querySelector("#rig")
         var pos = rig.getAttribute("position")
--- tardis-world/scripts/movement.js
+++ tardis-world/scripts/movement.js
@@ -15,3 +15,55 @@ AFRAME.registerComponent('thumbstick-logging',{
         rig.setAttribute("position", pos);
     }
 });
+
+function queueAdder(items, length, item){
+    if (items.length < length){
+        items.unshift(item)
+    }else{
+        items.unshift(item)
+        items.pop()
+    }
+    return items
+}
+
+
+function getTotalDistance(vals){
+    var dist = 0
+    for (let j = 1; i < vals.length; j++){
+        dist += MATH.abs(vals[0] - vals[j])
+    }
+    return dist
+}
+
+
+AFRAME.registerComponent('headbob-movement',{
+    init: function() {
+        this.y_positions = []
+        this.x_positions = []
+        this.z_positions = []
+        this.length = 20
+    },
+
+    tick: function () {
+        var rig = document.querySelector("#rig")
+        var player = document.querySelector("#camera")
+        var angle = player.getAttribute("rotation")
+
+        this.z_positions = queueAdder(this.z_positions,this.length, player.getAttribute('position').z)
+        this.x_positions = queueAdder(this.x_positions,this.length, player.getAttribute('position').x)
+
+        if(getTotalDistance(this.x_positions) < 0.20 && getTotalDistance(this.z_positions) < 0.20 ){
+
+            this.y_positions = queueAdder(this.y_positions,this.length, player.getAttribute('position').y)
+            var dist = getTotalDistance(this.y_positions)
+            if (dist > 0.2){
+                var x = (dist)/25 * Math.cos(angle.y * Math.PI / 180)
+                var y = (dist)/25 * Math.sin(angle.y * Math.PI / 180)
+                var pos = rig.getAttribute("position")
+                pos.x -= y;
+                pos.z -= x;
+                rig.setAttribute("position", pos);
+            }
+        }
+    }
+});
