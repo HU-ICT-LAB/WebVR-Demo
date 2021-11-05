a1f7cf3bb7d359aa10a9adcdefa3e4d1d56e9a88
rubenski0345@gmail.com <Bocuzawekon.99>
2021-11-05 15:55:11 +0100

    added a scoreboard which updates the score every 5 seconds

---
+++ ai-game/highscorepublisher.py
@@ -0,0 +1,61 @@
+import paho.mqtt.client as paho
+import json
+import operator
+
+broker = "broker.emqx.io"
+port = 1883
+
+
+def on_publish(client, userdata, result):  # create function for callback
+    print("data published \n")
+    pass
+
+
+
+
+
+
+client1 = paho.Client("gamemodespub")  # create client object
+
+def on_connect(client, userdata, flags, rc):
+    client1.subscribe("request_scoretopic")
+    client1.subscribe("request_updatescore")
+
+
+def updateleaderboard(new_score="5", new_name="anonymous"):
+    highscore_file = open("leaderboard.txt", "r")
+    readable_highscores = json.loads(highscore_file.readline())
+    if new_name in readable_highscores.keys():
+        if readable_highscores[new_name] < new_score:
+            readable_highscores[new_name] = new_score
+    readable_highscores = dict(sorted(readable_highscores.items(), key=operator.itemgetter(1), reverse=True))
+    fout = open("leaderboard.txt", "w")
+    fout.write(json.dumps(readable_highscores))
+    fout.close()
+    print(readable_highscores)
+
+def getleaderboard():
+    highscore_file = open("leaderboard.txt", "r")
+    highscorestring = ""
+    for score in highscore_file:
+        highscorestring = highscorestring + score
+    print("data:" + highscorestring)
+    return highscorestring
+
+def on_message(client, userdata, msg):
+    if msg.topic == "request_scoretopic":
+        client1.publish('hbo_ict_vr_game_score', getleaderboard())  # publish
+    if msg.topic == "request_updatescore":
+        updateleaderboard(msg.payload.decode("utf-8"))
+        client1.publish('hbo_ict_vr_game_score', getleaderboard())
+
+
+client1.on_publish = on_publish  # assign function to callback
+client1.on_connect = on_connect
+client1.on_message = on_message
+client1.connect(broker, port)  # establish connection
+running = True
+while running:
+    client1.loop()
+
+
