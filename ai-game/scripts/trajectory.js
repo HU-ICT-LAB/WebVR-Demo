0195b97a48333a908c0e71cab53e77960f32c945
AHipWhale <charlie.choffat@gmail.com>
2021-11-08 19:52:31 +0100

    Tried a different way because code was too heavy to run. Problem with getting the right positions, but the rest works.

--- ai-game/scripts/trajectory.js
+++ ai-game/scripts/trajectory.js
@@ -1,5 +1,4 @@
-function calculateSteps(pos1, pos2) {
-    //Calculate the steps to calculate the trajectory by two given positions
+function calculateTrajectory(pos1, pos2) {
     var pos = new THREE.Vector3();
     var vec1 = [pos1['x'], pos1['y'], pos1['z']]
     var vec2 = [pos2['x'], pos2['y'], pos2['z']]
@@ -18,27 +17,13 @@ function diff (num1, num2) {
     }
 }
 
+var lijst = [];
 var trigger = false;
-var del = false;
-var showNext = false;
-var run = true;
+var check = true;
 
 AFRAME.registerComponent('trajectory', {
     init: function () {
-        var right_controller = this.el.querySelector('#right_controller');
-        right_controller.addEventListener('triggerdown', function() {
-            trigger = true;
-        });
-
-        var left_controller = this.el.querySelector('#left_controller');
-        left_controller.addEventListener('triggerup', function() {
-            var spawnLine = document.querySelector('#lijn');
-            del = true;
-            spawnLine.setAttribute('line', {
-                'start': "0 0 0",
-                'end': "0 0 0"
-            })
-        });
+        this.lastTick = 0;
 
         var headset = this.el.querySelector("#camera");
         const textEntity = document.createElement('a-entity');
@@ -49,71 +34,100 @@ AFRAME.registerComponent('trajectory', {
         textEntity.setAttribute('scale', '0.5 0.5 0.5');
         textEntity.setAttribute('text', 'color', '#be1f1f');
         textEntity.setAttribute('id', 'tekst');
+
     },
+    tick: function (time) {
+        //Runs every 2 seconds
+        if (Math.round(time - this.lastTick) > 2000) {
+            this.lastTick = Math.round(time);
 
-    tick: function () {
-        var el = document.querySelector('#main');
-        if (run) {
-            //Shows coordinates of VR gear from 0.5 seconds time ago on the left side of the screen
-            if (trigger) {
-                //Place a box
-                var list = getPositions(this.el);
-                var obj_rc = list[1];
+            //Place a box
+            var el = document.querySelector('#main');
+            var list = getPositions(this.el);
+            var obj_rc = list[1];
 
-                var pos = new THREE.Vector3();
+            var pos = new THREE.Vector3();
+            pos.x = obj_rc['x'];
+            pos.y = obj_rc['y'];
+            pos.z = obj_rc['z'];
 
-                var spawnEl = document.createElement('a-box');
+            lijst.push(pos);
 
-                pos.x = obj_rc['x'];
-                pos.y = obj_rc['y'];
-                pos.z = obj_rc['z'];
+            var spawnEl = document.createElement('a-box');
 
-                spawnEl.setAttribute('position', pos);
-                spawnEl.setAttribute('scale', '0.025 0.025 0.025');
-                spawnEl.setAttribute('id', 'drawBox');
+            spawnEl.setAttribute('position', pos);
+            spawnEl.setAttribute('scale', '0.025 0.025 0.025');
+            spawnEl.setAttribute('id', 'drawBox');
+
+            // Append to scene.
+            el.sceneEl.appendChild(spawnEl);
+
+            console.log(this.lastTick, lijst);
+        }
+
+        if (lijst.length === 2) {
+            var box1 = lijst[0];
+            var box2 = lijst[1];
+
+            var dif = calculateTrajectory(box1, box2);
+
+            var trajectory = [box1];
+
+            var pos1 = new THREE.Vector3();
+            pos1.x += box1.x;
+            pos1.y += box1.y;
+            pos1.z += box1.z;
+
+            // Append to scene.
+            el.sceneEl.appendChild(spawnEl2);
+
+            console.log(diff(box1.x, trajectory[trajectory.length - 1].x));
+
+            var point = [];
+
+            point.push(Math.abs(1/dif.x))
+            point.push(Math.abs(1/dif.y))
+            point.push(Math.abs(1/dif.z))
+
+
+            var lengte_lijst = Math.round(Math.min(...point));
+            console.log(lengte_lijst);
+            if (lengte_lijst === Infinity) {
+                lengte_lijst = 1;
+            }
+
+            //Problem with this bit of code
+            for (let i = 0; i < lengte_lijst; i++) {
+                var newPos = new THREE.Vector3();
+                newPos.x = dif.x * i;
+                newPos.y = dif.y * i;
+                newPos.z = dif.z * i;
+
+                newPos.x = newPos.x + box1.x
+                newPos.y = newPos.y + box1.y
+                newPos.z = newPos.z + box1.z
+
+                // var textEntity = document.querySelector('#tekst');
+                // textEntity.setAttribute('text', 'value', "Lengte lijst: " + JSON.stringify(box1));
+
+                trajectory.push(newPos);
+            }
+
+            for (var a = 0; a < trajectory.length; a++) {
+                var spawnEl1 = document.createElement('a-sphere');
+
+                spawnEl1.setAttribute('position', trajectory[a]);
+                spawnEl1.setAttribute('scale', '0.025 0.025 0.025');
+                spawnEl1.setAttribute('id', 'einde');
 
                 // Append to scene.
-                el.sceneEl.appendChild(spawnEl);
-                trigger = false;
-                showNext = true;
+                el.sceneEl.appendChild(spawnEl1);
+            }
+            trajectory = []
+
+            if (lijst.length >= 2) {
+                lijst = [];
             }
         }
-
-        var boxes = document.querySelectorAll('#drawBox');
-        if (boxes.length > 2) {
-            //Delete all boxes
-            boxes.forEach(item => el.sceneEl.removeChild(item));
-            del = false;
-        }
-        if (del) {
-            boxes.forEach(item => el.sceneEl.removeChild(item));
-            del = false;
-        }
-
-        if (!run) {
-            if (boxes.length === 2 && showNext) {
-                var box1 = boxes[0].getAttribute('position');
-                var box2 = boxes[1].getAttribute('position');
-                var dif = calculateTrajectory(box1, box2);
-                var endpoint = {'x': box1.x, 'y': box1.y, 'z': box1.z};
-
-                while (diff(box1.x, endpoint.x) < 1 && diff(box1.y, endpoint.y) < 1 && diff(box1.z, endpoint.z) < 1) {
-                    endpoint.x = endpoint.x + dif.x;
-                    endpoint.y = endpoint.y + dif.y;
-                    endpoint.z = endpoint.z + dif.z;
-                }
-                var textEntity = document.querySelector('#tekst');
-                textEntity.setAttribute('text', 'value', "box2: " + JSON.stringify(box2) + "\nendPoint: " + JSON.stringify(endpoint));
-
-                var spawnLine = document.querySelector('#lijn');
-                spawnLine.setAttribute('line', {
-                    'start': box1,
-                    'end': endpoint
-                })
-            }
-            showNext = false;
-        }
-
-        run = !run;
     }
 })
