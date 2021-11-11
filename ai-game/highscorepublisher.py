0b3b3701d52033d64735b5ffaba6207977b152f4
rubenski0345@gmail.com <Bocuzawekon.99>
2021-11-11 14:53:43 +0100

    updated the documentation v2

--- ai-game/highscorepublisher.py
+++ ai-game/highscorepublisher.py
@@ -9,6 +9,9 @@ port = 1883
 def on_publish(client, userdata, result):  # create function for callback
     '''
     when data is published, this function will be ran and print data published string
+    :param client: the client object (client1 in our case)
+    :param userdata: the detailed information about the user
+    :param result: the message results
     '''
     print("data published \n")
     pass
@@ -18,6 +21,10 @@ client1 = paho.Client("gamemodespub")  # create client object
 def on_connect(client, userdata, flags, rc):
     '''
     when connected to the server, subscribe to the topics
+    :param client: the client object (client1 in our case)
+    :param userdata: the detailed information about the user
+    :param flags: gives more detailed information about message
+    :param rc: return code (0 is accepted, 1 is rejected)
     '''
     client1.subscribe("request_scoretopic")
     client1.subscribe("request_updatescore")
@@ -26,6 +33,8 @@ def on_connect(client, userdata, flags, rc):
 def updateleaderboard(new_score="5", new_name="anonymous"):
     '''
     adds the new score and it's name to the leaderboard.txt and delete the lowest value and then overwrites it
+    :param new_score: the score that needs to be put in leaderboard
+    :param new_name: the new_name that goes with the score as key of the dictionary
     '''
     print(new_name + ":" + new_score)
     highscore_file = open("leaderboard.txt", "r")
@@ -60,6 +69,9 @@ def getleaderboard():
 def on_message(client, userdata, msg):
     '''
     when a message is received, checkout it's topic to run the correct functions
+    :param client: the client object (client1 in our case)
+    :param userdata: the detailed information about the user
+    :param msg: message itself
     '''
     if msg.topic == "request_scoretopic":
         client1.publish('hbo_ict_vr_game_score', getleaderboard())  # publish
