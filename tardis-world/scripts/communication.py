073a04ab1cac35a7a68c19b8082cb473687700db
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-11-11 13:21:58 +0100

    Added more comments, renamed file, and added speed controll to index

--- tardis-world/scripts/communication.py
+++ tardis-world/scripts/communication.py
@@ -1,41 +1,38 @@
+# This python code is used to controll the URE3 robot arm
 import socket
 import time
 
+# Ip adress and port of the robot arm
 HOST = "192.168.137.232"
 PORT = 30002
 
+# create a socket connection to the robot arm
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+# wait two seconds for connection to be finished
 time.sleep(2)
 
+# send a command to move the robot joins to orientation [0, 0, 0, 0, 0, -180] degrees
 s.send((" movej([0 ,0, 0 ,0  ,0 ,-3.14], a=1.4, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
 
+# Wait 5 seconds for move to be completed
 time.sleep(5)
 
+# send a command to move the robot joins to orientation [0, -90, 90, -90, -90, -180] degrees
 s.send((" movej([0 ,-1.57, 1.57 ,-1.57 ,-1.57 ,-3.14], a=1.4, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
 
+# Wait 5 seconds for move to be completed
 time.sleep(5)
 
-# s.send((" pos:=get_actual_tcp_pose()"+"\n").encode("utf8"))
-# s.send((" socket_send_string(pos)"+"\n").encode("utf8"))
-
-
-# s.send((" pose1 = "+"\n").encode("utf8"))
-# s.send((" pose2 = "+"\n").encode("utf8"))
-print("move forward")
-
+# Send a command to move the robot arm relative to the current position, we move it 20cm on the x and -20 on the z axis
 s.send((" movej(pose_trans(get_forward_kin(), p[0.2, -0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
 
+# Wait 5 seconds for move to be completed
 time.sleep(5)
 
-# s.send((" pose1 = "+"\n").encode("utf8"))
-# s.send((" pose2 = "+"\n").encode("utf8"))
-print("move back")
+# Send a command to move the robot arm relative to the current position, we move it -20cm on the x and 20 on the z axis
 s.send((" movej(pose_trans(get_forward_kin(), p[-0.2, 0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
 
-# data = s.recv(2048)
-# print(data)
-
-
+# Close the socket connection
 s.close()
