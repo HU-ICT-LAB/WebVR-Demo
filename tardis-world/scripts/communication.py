d7a426634cc44d105318df3e36ad123353487680
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-11-15 16:28:36 +0100

    can open and close gripper remoteley, but its slow and unstable

--- tardis-world/scripts/communication.py
+++ tardis-world/scripts/communication.py
@@ -3,12 +3,36 @@ import socket
 import time
 
 # Ip adress and port of the robot arm
-HOST = "192.168.137.232"
+HOST = "192.168.137.176"
 PORT = 30002
 
 # create a socket connection to the robot arm
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+
+def gripper_open():
+    f = open("Gripper.script", "rb")  # Robotiq Gripper
+    # f = open ("setzero.script", "rb")  #Robotiq FT sensor
+
+    l = f.read()
+    while (l):
+        s.send(l)
+        l = f.read(1024)
+    s.send(" rq_open()\nend".encode("utf8"))
+
+
+def gripper_close():
+    f = open("Gripper.script", "rb")  # Robotiq Gripper
+    # f = open ("setzero.script", "rb")  #Robotiq FT sensor
+
+    l = f.read()
+    while (l):
+        s.send(l)
+        l = f.read(1024)
+    s.send(" rq_close()\nend".encode("utf8"))
+
+
 
 # wait two seconds for connection to be finished
 time.sleep(2)
@@ -28,11 +52,31 @@ time.sleep(5)
 # Send a command to move the robot arm relative to the current position, we move it 20cm on the x and -20 on the z axis
 s.send((" movej(pose_trans(get_forward_kin(), p[0.2, -0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
 
-# Wait 5 seconds for move to be completed
+time.sleep(5)
+
+gripper_open()
+
+time.sleep(10)
+
+s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, 0.08, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+
+time.sleep(5)
+
+gripper_close()
+
+time.sleep(10)
+
+s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, -0.08, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+
 time.sleep(5)
 
 # Send a command to move the robot arm relative to the current position, we move it -20cm on the x and 20 on the z axis
 s.send((" movej(pose_trans(get_forward_kin(), p[-0.2, 0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+
+
+
+
+
 
 # Close the socket connection
 s.close()
