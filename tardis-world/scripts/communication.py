5ba553d432af58813f19deab2d20e681b4ad94b3
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-11-25 16:25:33 +0100

    Added buttons to controll the robot arm around the user

--- tardis-world/scripts/communication.py
+++ tardis-world/scripts/communication.py
@@ -1,79 +1,145 @@
 # This python code is used to controll the URE3 robot arm
 import socket
 import time
+import threading, queue
+
+import paho.mqtt.client as paho
+import json
+import operator
 
 # Ip adress and port of the robot arm
-HOST = "192.168.1.106"
-PORT = 30002
+HOST = "192.168.137.162"
+ROBOT_PORT = 30002
 
 # create a socket connection to the robot arm
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect((HOST, ROBOT_PORT))
 
 
-def gripper_open():
+def move_backwards():
+    s.send((" movej(pose_trans(get_forward_kin(), p[0, -0.05, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+def move_forwards():
+    s.send((" movej(pose_trans(get_forward_kin(), p[0, 0.05, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+def move_left():
+    s.send((" movej(pose_trans(get_forward_kin(), p[0.05, 0, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+def move_right():
+    s.send((" movej(pose_trans(get_forward_kin(), p[-0.05, 0, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+def move_up():
+    print("moving up")
+    s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, -0.05, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+def move_down():
+    s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, 0.05, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+    time.sleep(2)
+
+
+def move_open():
     f = open("Gripper.script", "rb")  # Robotiq Gripper
     l = f.read()
     while (l):
         s.send(l)
         l = f.read(1024)
     s.send(" rq_open()\nend\n".encode("utf8"))
-    print("sending is done")
+    time.sleep(5)
 
-
-def gripper_close():
+def move_close():
     f = open("Gripper.script", "rb")  # Robotiq Gripper
     l = f.read()
     while (l):
         s.send(l)
         l = f.read(1024)
     s.send(" rq_close()\nend\n".encode("utf8"))
+    time.sleep(5)
+
+
+# ###############################################################################################################################################################
 
 
 
-# wait two seconds for connection to be finished
-time.sleep(2)
+# broker server:
+broker = "broker.emqx.io"
+port = 1883
+client1 = paho.Client("ROBOT_Controller")  # create client object
+running = True
 
-# send a command to move the robot joins to orientation [0, 0, 0, 0, 0, -180] degrees
-s.send((" movej([0 ,0, 0 ,0  ,0 ,-3.14], a=1.4, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
-
-# Wait 5 seconds for move to be completed
-time.sleep(5)
-
-# send a command to move the robot joins to orientation [0, -90, 90, -90, -90, -180] degrees
-s.send((" movej([0 ,-1.57, 1.57 ,-1.57 ,-1.57 ,-3.14], a=1.4, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
-
-# Wait 5 seconds for move to be completed
-time.sleep(5)
-
-# Send a command to move the robot arm relative to the current position, we move it 20cm on the x and -20 on the z axis
-s.send((" movej(pose_trans(get_forward_kin(), p[0.2, -0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
-
-time.sleep(5)
-
-gripper_open()
-
-time.sleep(5)
-
-s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, 0.08, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
-
-time.sleep(5)
-
-gripper_close()
-
-time.sleep(5)
-
-s.send((" movej(pose_trans(get_forward_kin(), p[0, 0, -0.08, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
-
-time.sleep(5)
-
-# Send a command to move the robot arm relative to the current position, we move it -20cm on the x and 20 on the z axis
-s.send((" movej(pose_trans(get_forward_kin(), p[-0.2, 0.2, 0, 0, 0, 0]), a=1.2, v=1.05, t=0, r=0)"+"\n").encode("utf8"))
+movement_queue = queue.Queue()
 
 
+def on_connect(client, userdata, flags, rc):
+    """
+    when connected to the server, subscribe to the topics
+    :param client: the client object (client1 in our case)
+    :param userdata: the detailed information about the user
+    :param flags: gives more detailed information about message
+    :param rc: return code (0 is accepted, 1 is rejected)
+    """
+    client1.subscribe("hbo_ict_robot_arm_controll")
+
+def on_message(client, userdata, msg):
+    """
+    when a message is received, checkout it's topic to run the correct functions
+    :param client: the client object (client1 in our case)
+    :param userdata: the detailed information about the user
+    :param msg: message itself
+    """
+
+    if msg.topic == "hbo_ict_robot_arm_controll":
+        print("msg recieved")
+        print(msg.payload)
+        payload = json.loads(msg.payload)
+        movement_queue.put( payload['command'])
+
+def movement_thread():
+    print("started thread")
+    while True:
+        move = movement_queue.get()
+        print(move)
+        if move == "move_up":
+            move_up()
+        elif move == "move_down":
+            move_down()
+        elif move == "move_left":
+            move_left()
+        elif move == "move_right":
+            move_right()
+        elif move == "move_forward":
+            move_forwards()
+        elif move == "move_backward":
+            move_backwards()
+        elif move == "move_open":
+            move_open()
+        elif move == "move_close":
+            move_close()
+        elif move == "shutdown":
+            running = False
+            break
+        else:
+            pass
 
 
 
+if __name__ == "__main__":
+    # code to connect to the server and which message is connect to which function
 
-# Close the socket connection
-s.close()
+    client1.on_connect = on_connect
+    client1.on_message = on_message
+    client1.connect(broker, port)  # establish connection
+
+    # let is run until manual interruption
+    x = threading.Thread(target=movement_thread, args=(), daemon=True)
+    x.start()
+
+    print("running main")
+    while running:
+        client1.loop()
+
+    x.join()
