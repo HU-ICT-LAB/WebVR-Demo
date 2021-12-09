1f9c1e14d779fe2b02b41ab712cddead379c9186
Floris Videler <floris@relevantonline.eu>
2021-12-09 11:40:48 +0100

    Cleaner code; #219; #218;

--- world-builder/scripts/main.js
+++ world-builder/scripts/main.js
@@ -1,274 +1,276 @@
 
 
 
-function sendLogToServer(msg){
-  client.publish("world-builder-debug", JSON.stringify(msg))
+function sendLogToServer(msg) {
+    client.publish("world-builder-debug", JSON.stringify(msg))
 }
 
 
 
 // Add objects to the a scene by pressing right hand controller trigger.
 AFRAME.registerComponent('add-object', {
-  events: {
-      click: function (e) {
-          const scene = document.querySelector('a-scene');
-          const newEl = document.createElement('a-entity');
-          const position = e.detail.intersection.point;
-          
-          newEl.setAttribute('position', position);
-          newEl.setAttribute('mixin', 'voxel');
-          newEl.setAttribute('class', 'collidable block');
-          
-          scene.appendChild(newEl);
-      }
-  }
+    events: {
+        click: function (e) {
+            const scene = document.querySelector('a-scene');
+            const newEl = document.createElement('a-entity');
+            const position = e.detail.intersection.point;
+
+            newEl.setAttribute('position', position);
+            newEl.setAttribute('mixin', 'voxel');
+            newEl.setAttribute('class', 'collidable block');
+
+            scene.appendChild(newEl);
+        }
+    }
 });
 
 /* Snaps objects together by getting the position of the an object
 and make a grid and add an object next to the object. */
 AFRAME.registerComponent('snap', {
-  dependencies: ['position'],
+    dependencies: ['position'],
 
-  schema: {
-    offset: {type: 'vec3'},
-    snap: {type: 'vec3'}
-  },
+    schema: {
+        offset: { type: 'vec3' },
+        snap: { type: 'vec3' }
+    },
 
-  init: function () {
-    this.originalPos = this.el.getAttribute('position');
-  },
+    init: function () {
+        this.originalPos = this.el.getAttribute('position');
+    },
 
-  update: function () {
-    const data = this.data;
+    update: function () {
+        const data = this.data;
 
-    const pos = AFRAME.utils.clone(this.originalPos);
-    pos.x = Math.floor(pos.x / data.snap.x) * data.snap.x + data.offset.x;
-    pos.y = Math.floor(pos.y / data.snap.y) * data.snap.y + data.offset.y;
-    pos.z = Math.floor(pos.z / data.snap.z) * data.snap.z + data.offset.z;
+        const pos = AFRAME.utils.clone(this.originalPos);
+        pos.x = Math.floor(pos.x / data.snap.x) * data.snap.x + data.offset.x;
+        pos.y = Math.floor(pos.y / data.snap.y) * data.snap.y + data.offset.y;
+        pos.z = Math.floor(pos.z / data.snap.z) * data.snap.z + data.offset.z;
 
-    if (pos.y < 0) {
-      pos.y = data.offset.y;
+        if (pos.y < 0) {
+            pos.y = data.offset.y;
+        }
+
+        this.el.setAttribute('position', pos);
     }
-
-    this.el.setAttribute('position', pos);
-  }
 });
 
 // delete object
 AFRAME.registerComponent('delete-object', {
-  events: {
-    click: function(e) {
-      let obj = e.detail.intersection.object.el;
-      obj.parentNode.removeChild(obj);
+    events: {
+        click: function (e) {
+            let obj = e.detail.intersection.object.el;
+            obj.parentNode.removeChild(obj);
+        }
     }
-  }
 });
 
 
 
-
-/* TERRAIN STUFF */
-
+// Global variable for moving the terrain on the y-axis.
 let GLOBALMIN = 1
 
+/* Aframe geometry for making custom terrain */
 AFRAME.registerGeometry('terrain', {
-  init: function (data) {
-      console.log(data);
-      let WIDTH = 250;
-      let HEIGHT = 250;
-      let SIZE_AMPLIFIER = 5;
-      let HEIGHT_AMPLIFIER = 1;
-      var plane = new THREE.PlaneBufferGeometry(WIDTH * SIZE_AMPLIFIER, HEIGHT * SIZE_AMPLIFIER, WIDTH - 1, HEIGHT - 1);
-      var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide, shading: THREE.FlatShading });
+    init: function (data) {
 
-      // TODO remove canvas. Gen terrain without
-      var canv = document.createElement('canvas');
-      canv.width = "250"
-      canv.height = "250"
-      let pv = 1
-      let kv = 0.2
-      let ctx = canv.getContext("2d");
-      let generalArray = [];
-      let max = 0;
-      let min = 5;
-      let totalInPlayableArea = 0;
-      let avgInPlayableArea = 0;
-      let c  = 0;
-      for (let y = 0; y < HEIGHT; y++) {
-          let rowArray = [];
-          for (let x = 0; x < WIDTH; x++) {
-              let n = noise2D(x * 0.01, y * 0.01);
-              n += pv;
-              n *= kv;
-              if (n > max) {
-                  max = n;
-              }
-              if (n < min) {
-                  min = n;
-              }
-              // The flatspot.
-              if ((y > 75 && y < 175) && (x > 75 && x < 175)) {
-                  c += 1
-                  let n = noise2D(x * 0.01, y * 0.01);
-                  n += pv;
-                  n *= kv;
-                  totalInPlayableArea += n;
-                  n = 0;
-              }
-              rowArray.push(n);
-              
-          }
-          generalArray.push(rowArray);
-      }
+        // Some start variables
+        let WIDTH = 250;
+        let HEIGHT = 250;
+        let SIZE_AMPLIFIER = 5;
+        let HEIGHT_AMPLIFIER = 1;
+        var plane = new THREE.PlaneBufferGeometry(WIDTH * SIZE_AMPLIFIER, HEIGHT * SIZE_AMPLIFIER, WIDTH - 1, HEIGHT - 1);
+        var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide, shading: THREE.FlatShading });
 
-      avgInPlayableArea = totalInPlayableArea / c;
-      console.log('avg: '+ avgInPlayableArea);
+        // TODO remove canvas. Gen terrain without
+        var canv = document.createElement('canvas');
+        canv.width = WIDTH
+        canv.height = HEIGHT
+        let pv = 1
+        let kv = 0.2
+        let ctx = canv.getContext("2d");
+        let generalArray = [];
+        let max = 0;
+        let min = 5;
+        let totalInPlayableArea = 0;
+        let avgInPlayableArea = 0;
+        let c = 0;
+        for (let y = 0; y < HEIGHT; y++) {
+            let rowArray = [];
+            for (let x = 0; x < WIDTH; x++) {
+                let n = noise2D(x * 0.01, y * 0.01);
+                n += pv;
+                n *= kv;
+                if (n > max) {
+                    max = n;
+                }
+                if (n < min) {
+                    min = n;
+                }
+                // The flatspot.
+                if ((y > 75 && y < 175) && (x > 75 && x < 175)) {
+                    c += 1
+                    let n = noise2D(x * 0.01, y * 0.01);
+                    n += pv;
+                    n *= kv;
+                    totalInPlayableArea += n;
+                    n = 0;
+                }
+                rowArray.push(n);
 
+            }
+            generalArray.push(rowArray);
+        }
 
-      let minN = 1;
-      y = 75
-      for (x = 75; x < 175; x++){
-          edgeN = generalArray[y][x];
-          if (edgeN <  minN){
-              minN = edgeN;
-          }
-      }
-      y = 175
-      for (x = 75; x < 175; x++){
-          edgeN = generalArray[y][x];
-          if (edgeN <  minN){
-              minN = edgeN;
-          }
-      }
-      x = 75
-      for (y = 75; y < 175; y++){
-          edgeN = generalArray[y][x];
-          if (edgeN <  minN){
-              minN = edgeN;
-          }
-      }
-      x = 175
-      for (y = 75; y < 75; y++){
-          edgeN = generalArray[y][x];
-          if (edgeN <  minN){
-              minN = edgeN;
-          }
-      }
-      console.log(minN);
-      GLOBALMIN = minN;
-
-      for(let y = 75; y < 175; y++){
-          for(let x = 75; x < 175; x++){
-              generalArray[y][x] = minN;
-          }
-      }
+        avgInPlayableArea = totalInPlayableArea / c;
+        console.log('avg: ' + avgInPlayableArea);
 
 
-      let upperEdge = 75;
-      let rightEdge = 175;
-      let lowerEdge = 175;
-      let leftEdge = 75;
-
-      let smooth = false;
-
-      while (!smooth) {
-          for (let x = 75; x < 175; x++){
-              nextN = generalArray[upperEdge-1][x];
-              total = nextN + generalArray[upperEdge][x];
-              generalArray[upperEdge][x] = total / 2;
-
-          }
-
-          for (let y = 75; y < 175; y++){
-              nextN = generalArray[y][rightEdge+1];
-              total = nextN + generalArray[y][rightEdge];
-              generalArray[y][rightEdge] = total / 2;
-
-          }
-
-          for (let x = 75; x < 175; x++){
-              nextN = generalArray[lowerEdge+1][x];
-              total = nextN + generalArray[lowerEdge][x];
-              generalArray[lowerEdge][x] = total / 2;
-
-          }
-
-          for (let y = 75; y < 175; y++){
-              nextN = generalArray[y][leftEdge-1];
-              total = nextN + generalArray[y][leftEdge];
-              generalArray[y][leftEdge] = total / 2;
-
-          }
-          upperEdge++;
-          rightEdge--;
-          lowerEdge--;
-          leftEdge++;
-
-          if(upperEdge == 122){
-              smooth = true;
-          } 
-      }
+        // Find the global min height.
+        let minN = 1;
+        y = 75
+        for (x = 75; x < 175; x++) {
+            edgeN = generalArray[y][x];
+            if (edgeN < minN) {
+                minN = edgeN;
+            }
+        }
+        y = 175
+        for (x = 75; x < 175; x++) {
+            edgeN = generalArray[y][x];
+            if (edgeN < minN) {
+                minN = edgeN;
+            }
+        }
+        x = 75
+        for (y = 75; y < 175; y++) {
+            edgeN = generalArray[y][x];
+            if (edgeN < minN) {
+                minN = edgeN;
+            }
+        }
+        x = 175
+        for (y = 75; y < 75; y++) {
+            edgeN = generalArray[y][x];
+            if (edgeN < minN) {
+                minN = edgeN;
+            }
+        }
+        
+        // Set the blobal min height.
+        GLOBALMIN = minN;
+        // Make sure the flatspot is on the min height level.
+        for (let y = 75; y < 175; y++) {
+            for (let x = 75; x < 175; x++) {
+                generalArray[y][x] = minN;
+            }
+        }
 
 
-      for(let y = 0; y < 250; y++){
-          for(let x = 0; x < 250; x++){
-              let n = generalArray[y][x];
-              let rgb = Math.round(255*n);
-              ctx.fillStyle = "rgba("+rgb+","+rgb+","+rgb+",1.0)";
-              ctx.fillRect(x, y, 1, 1);
-          }
-      }
+        // do some light smoothing
+        let upperEdge = 75;
+        let rightEdge = 175;
+        let lowerEdge = 175;
+        let leftEdge = 75;
+
+        let smooth = false;
+
+        while (!smooth) {
+            for (let x = 75; x < 175; x++) {
+                nextN = generalArray[upperEdge - 1][x];
+                total = nextN + generalArray[upperEdge][x];
+                generalArray[upperEdge][x] = total / 2;
+
+            }
+
+            for (let y = 75; y < 175; y++) {
+                nextN = generalArray[y][rightEdge + 1];
+                total = nextN + generalArray[y][rightEdge];
+                generalArray[y][rightEdge] = total / 2;
+
+            }
+
+            for (let x = 75; x < 175; x++) {
+                nextN = generalArray[lowerEdge + 1][x];
+                total = nextN + generalArray[lowerEdge][x];
+                generalArray[lowerEdge][x] = total / 2;
+
+            }
+
+            for (let y = 75; y < 175; y++) {
+                nextN = generalArray[y][leftEdge - 1];
+                total = nextN + generalArray[y][leftEdge];
+                generalArray[y][leftEdge] = total / 2;
+
+            }
+            upperEdge++;
+            rightEdge--;
+            lowerEdge--;
+            leftEdge++;
+
+            if (upperEdge == 122) {
+                smooth = true;
+            }
+        }
+
+        // Fill in the canvas.
+        for (let y = 0; y < 250; y++) {
+            for (let x = 0; x < 250; x++) {
+                let n = generalArray[y][x];
+                let rgb = Math.round(255 * n);
+                ctx.fillStyle = "rgba(" + rgb + "," + rgb + "," + rgb + ",1.0)";
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
 
 
 
-      data = canv.getContext("2d").getImageData(0, 0, WIDTH, HEIGHT).data;
+        data = canv.getContext("2d").getImageData(0, 0, WIDTH, HEIGHT).data;
 
-      // Generate the actual terrain.
-      var vertices = plane.attributes.position.array;
-      for (i = 0, j = 2; i < data.length; i += 4, j += 3) {
-          vertices[j] = data[i] * HEIGHT_AMPLIFIER;
-      }
+        // Generate the actual terrain from the canvas.
+        var vertices = plane.attributes.position.array;
+        for (i = 0, j = 2; i < data.length; i += 4, j += 3) {
+            vertices[j] = data[i] * HEIGHT_AMPLIFIER;
+        }
 
-      var mesh = new THREE.Mesh(plane, material);
+        var mesh = new THREE.Mesh(plane, material);
 
-      plane.computeFaceNormals();
-      plane.computeVertexNormals();
+        plane.computeFaceNormals();
+        plane.computeVertexNormals();
 
-      // GLOBALMIN = minPixelAroundFlatSpot
-      console.log(plane);
-      this.geometry = plane;
-  }
+        console.log(plane);
+        this.geometry = plane;
+    }
 });
 
 // Set the position of the terrain so it is on playable height.
 AFRAME.registerComponent('set-p', {
-  dependencies: ['position'],
-  
-  init: function () {
-      pos = {'x': 0, 'y': -(GLOBALMIN * 5 * 10)-.2, 'z': 0}
-      this.el.setAttribute('position', pos);
-  }
+    dependencies: ['position'],
+
+    init: function () {
+        pos = { 'x': 0, 'y': -(GLOBALMIN * 5 * 10) - .2, 'z': 0 }
+        this.el.setAttribute('position', pos);
+    }
 });
 
 
 class Vector2 {
-  /**
-   * 
-   * @param {number} x 
-   * @param {number} y 
-   */
-  constructor(x, y) {
-      this.x = x;
-      this.y = y;
-  }
-  /**
-   * 
-   * @param {Vector2} other 
-   * @returns Dot product.
-   */
-  dot(other) {
-      return this.x * other.x + this.y * other.y;
-  }
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * 
+     * @param {Vector2} other 
+     * @returns Dot product.
+     */
+    dot(other) {
+        return this.x * other.x + this.y * other.y;
+    }
 }
 
 /**
@@ -276,13 +278,13 @@ class Vector2 {
 * @param {number} tab 
 */
 function shuffle(tab) {
-  for (let e = tab.length - 1; e > 0; e--) {
-      let index = Math.round(Math.random() * (e - 1)),
-          temp = tab[e];
+    for (let e = tab.length - 1; e > 0; e--) {
+        let index = Math.round(Math.random() * (e - 1)),
+            temp = tab[e];
 
-      tab[e] = tab[index];
-      tab[index] = temp;
-  }
+        tab[e] = tab[index];
+        tab[index] = temp;
+    }
 }
 
 /**
@@ -290,16 +292,16 @@ function shuffle(tab) {
 * @returns Permutation array.
 */
 function makePermutation() {
-  let P = [];
-  for (let i = 0; i < 256; i++) {
-      P.push(i);
-  }
-  shuffle(P);
-  for (let i = 0; i < 256; i++) {
-      P.push(P[i]);
-  }
+    let P = [];
+    for (let i = 0; i < 256; i++) {
+        P.push(i);
+    }
+    shuffle(P);
+    for (let i = 0; i < 256; i++) {
+        P.push(P[i]);
+    }
 
-  return P;
+    return P;
 }
 let P = makePermutation();
 
@@ -309,16 +311,16 @@ let P = makePermutation();
 * @returns Vector2
 */
 function getConstantVector(v) {
-  //v is the value from the permutation table.
-  let h = v & 3;
-  if (h == 0)
-      return new Vector2(1.0, 1.0);
-  else if (h == 1)
-      return new Vector2(-1.0, 1.0);
-  else if (h == 2)
-      return new Vector2(-1.0, -1.0);
-  else
-      return new Vector2(1.0, -1.0);
+    //v is the value from the permutation table.
+    let h = v & 3;
+    if (h == 0)
+        return new Vector2(1.0, 1.0);
+    else if (h == 1)
+        return new Vector2(-1.0, 1.0);
+    else if (h == 2)
+        return new Vector2(-1.0, -1.0);
+    else
+        return new Vector2(1.0, -1.0);
 }
 
 /**
@@ -327,7 +329,7 @@ function getConstantVector(v) {
 * @returns number
 */
 function fade(t) {
-  return ((6 * t - 15) * t + 10) * t * t * t;
+    return ((6 * t - 15) * t + 10) * t * t * t;
 }
 
 /**
@@ -338,7 +340,7 @@ function fade(t) {
 * @returns number
 */
 function lerp(t, a1, a2) {
-  return a1 + t * (a2 - a1);
+    return a1 + t * (a2 - a1);
 }
 
 /**
@@ -348,34 +350,34 @@ function lerp(t, a1, a2) {
 * @returns {number} A random value
 */
 function noise2D(x, y) {
-  let X = Math.floor(x) & 255;
-  let Y = Math.floor(y) & 255;
+    let X = Math.floor(x) & 255;
+    let Y = Math.floor(y) & 255;
 
-  let xf = x - Math.floor(x);
-  let yf = y - Math.floor(y);
+    let xf = x - Math.floor(x);
+    let yf = y - Math.floor(y);
 
-  let topRight = new Vector2(xf - 1.0, yf - 1.0);
-  let topLeft = new Vector2(xf, yf - 1.0);
-  let bottomRight = new Vector2(xf - 1.0, yf);
-  let bottomLeft = new Vector2(xf, yf);
+    let topRight = new Vector2(xf - 1.0, yf - 1.0);
+    let topLeft = new Vector2(xf, yf - 1.0);
+    let bottomRight = new Vector2(xf - 1.0, yf);
+    let bottomLeft = new Vector2(xf, yf);
 
-  //Select a value in the array for each of the 4 corners.
-  let valueTopRight = P[P[X + 1] + Y + 1];
-  let valueTopLeft = P[P[X] + Y + 1];
-  let valueBottomRight = P[P[X + 1] + Y];
-  let valueBottomLeft = P[P[X] + Y];
+    //Select a value in the array for each of the 4 corners.
+    let valueTopRight = P[P[X + 1] + Y + 1];
+    let valueTopLeft = P[P[X] + Y + 1];
+    let valueBottomRight = P[P[X + 1] + Y];
+    let valueBottomLeft = P[P[X] + Y];
 
-  let dotTopRight = topRight.dot(getConstantVector(valueTopRight));
-  let dotTopLeft = topLeft.dot(getConstantVector(valueTopLeft));
-  let dotBottomRight = bottomRight.dot(getConstantVector(valueBottomRight));
-  let dotBottomLeft = bottomLeft.dot(getConstantVector(valueBottomLeft));
+    let dotTopRight = topRight.dot(getConstantVector(valueTopRight));
+    let dotTopLeft = topLeft.dot(getConstantVector(valueTopLeft));
+    let dotBottomRight = bottomRight.dot(getConstantVector(valueBottomRight));
+    let dotBottomLeft = bottomLeft.dot(getConstantVector(valueBottomLeft));
 
-  let u = fade(xf);
-  let v = fade(yf);
+    let u = fade(xf);
+    let v = fade(yf);
 
-  return lerp(u,
-      lerp(v, dotBottomLeft, dotTopLeft),
-      lerp(v, dotBottomRight, dotTopRight)
-  );
+    return lerp(u,
+        lerp(v, dotBottomLeft, dotTopLeft),
+        lerp(v, dotBottomRight, dotTopRight)
+    );
 
 }
