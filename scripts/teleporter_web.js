6c036bef1cae509877b5e39329b5c7b07d7a7ab8
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-12-23 17:12:45 +0100

    Designed the hub, with a lot of models minor changes to web teleporter

--- scripts/teleporter_web.js
+++ scripts/teleporter_web.js
@@ -8,12 +8,13 @@ var teleporting = false
 AFRAME.registerComponent('web_teleporter',{
     /**
      * teleporter_id the id of this teleporter
-     * targer_id the id of the target teleporter/ to wich teleporter do you want to teleport.
+     * target_link: the desired webpage to teleport to
+     * target_text: the desired text to show above the button
      */
     schema: {
         teleporter_id: {default: 0},
-        target_id: {default: 1},
-        target_link: {default: ""}
+        target_link: {default: ""},
+        target_text: {default: ""}
       },
     /**
      * Initialisation function of the component
@@ -75,6 +76,15 @@ AFRAME.registerComponent('web_teleporter',{
         button.setAttribute("position", "0 1.05 0")
         this.el.appendChild(button)
 
+        var text = document.createElement("a-text")
+        text.setAttribute("id", "teleporter_" + this.data.teleporter_id + "_text")
+        text.setAttribute("value", this.data.target_text)
+        text.setAttribute("align", "center")
+        text.setAttribute("position", "0 1.2 0")
+        text.setAttribute("scale", "0.7 0.7 0.7")
+        text.setAttribute("material", "side: double")
+        this.el.appendChild(text)
+
     },
 
     /**
@@ -89,21 +99,9 @@ AFRAME.registerComponent('web_teleporter',{
         }.bind(this), 2000);
     },
 
-    // /**
-    //  * This function starts the multiple stages of the end animation: player_recieved and player_recieved2
-    //  */
-    // end_animation: function(){
-    //     var tub = this.el.querySelector("#tube_"+ this.data.teleporter_id)
-    //     tub.emit("player_recieved")
-    //     setTimeout(function() {
-    //         tub.emit("player_recieved_2")
-    //         teleporting = false
-    //     }.bind(this), 2000);
-    // },
-
 
     /**
-     * This function starts the teleportation sequence. It finds the correct target teleporter using the target_id and starts the correct events at the right time.
+     * This function starts the teleportation sequence starts the correct animation events at the right time.
      */
     teleportsequence: function(){
         if( !teleporting){
@@ -117,8 +115,7 @@ AFRAME.registerComponent('web_teleporter',{
     },
 
     /**
-     * this function teleports the player. It gets the location of the current teleporter and the target teleporter. This difference is added onto the rig of the player.
-     * This way the player doesnt end up in the middle of the teleporter but on the same spot as the one he teleported from. 
+     * this function teleports the player to the new web page
      */
     teleportPlayer: function(){
         console.log(this.data.target_link)
@@ -141,14 +138,33 @@ AFRAME.registerPrimitive('a-web-teleporter', {
     // Maps HTML attributes to the `teleporter` component properties
     mappings: {
       teleporter_id: 'web_teleporter.teleporter_id',
-      target_id: 'web_teleporter.target_id',
-      target_link: 'web_teleporter.target_link'
+      target_link: 'web_teleporter.target_link',
+      target_text: "web_teleporter.target_text"
     }
 });
 
+
+/**
+ * An component to change the target link and text of a web-teleporter
+ */
 AFRAME.registerComponent('link_selector',{
+    /**
+     * links:  a list of links "/path/to/file.html,/path/to/otherfile.html" sperated by a ,
+     * texts: a list of strings "i am a string, i am another string" seperated by a ,
+     * teleporter_id the id of the teleporter you want to controll
+     * button_color: the color of the button
+     * base_color: the color of the base of the button
+     * changer_id: the id of this link_selector
+     */
     schema: {
         links: {
+            type: 'string',
+            default: [""],
+            parse: function (value) {
+                return value.split(',');
+            },
+        },
+        texts: {
             type: 'string',
             default: [""],
             parse: function (value) {
@@ -161,8 +177,12 @@ AFRAME.registerComponent('link_selector',{
         changer_id: {default: 0}
     },
 
+    /**
+     * Initializes the component
+     */
     init: function(){
         this.index = 0
+        this.text_index = 0
 
         this.el.setAttribute("button_listener", "button_channel:  web_teleporter_links_"+ this.data.changer_id)
         this.el.addEventListener("web_teleporter_linkchange", this.change_portal.bind(this))
@@ -177,6 +197,10 @@ AFRAME.registerComponent('link_selector',{
 
     },
 
+    /**
+     * Changes the link and text on the selected portal,
+     * If multiple links are given, it will cycle thru the links and texts
+     */
     change_portal: function(){
         var TeleporterList = this.el.sceneEl.querySelectorAll("[web_teleporter]")
         var Destteleporter
@@ -186,10 +210,15 @@ AFRAME.registerComponent('link_selector',{
                 break
             }
         }
-        console.log(this.data.links)
-
         Destteleporter.setAttribute("web_teleporter", "target_link: "+ this.data.links[this.index])
-        
+
+        var text_entity = Destteleporter.querySelector("a-text")
+        text_entity.setAttribute("value", this.data.texts[this.text_index])
+
+        this.text_index += 1
+        if (this.list_index == this.data.texts.length){
+            this.text_index = 0
+        }
         this.index += 1
         if (this.index == this.data.links.length){
             this.index = 0
@@ -212,6 +241,7 @@ AFRAME.registerPrimitive('a-webtel-linkselector', {
     mappings: {
       teleporter_id: 'link_selector.teleporter_id',
       links: 'link_selector.links',
+      texts: 'link_selector.texts',
       button_color: 'link_selector.button_color',
       base_color: 'link_selector.base_color',
       changer_id: 'link_selector.changer_id'
